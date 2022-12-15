f2cb9e3a590ebe4c40e6ff185a6d97350a2dbf62
Ilya Shchutskiy <i1ya1l@yandex.ru>
2022-12-15 13:42:15 +0300

    feat(*): knexfile add pg clien

--- knexfile.js
+++ knexfile.js
@@ -40,17 +40,14 @@ export const staging = {
 };
 
 export const production = {
-  client: 'postgresql',
+  client: 'pg',
   connection: {
-    database: 'my_db',
-    user: 'username',
-    password: 'password',
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
   },
-  pool: {
-    min: 2,
-    max: 10,
-  },
-  migrations: {
-    tableName: 'knex_migrations',
-  },
+  migrations,
+  seeds,
 };
